5ec6e39282a809c10c2eacc862fbcbd39c2c8541
ElderSam <eldersamuel98@gmail.com>
2020-11-01 13:37:31 -0300

    aplicando estilos no Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+
+import GitHubLogo from '../assets/github-logo.png';
 import "./Header.css";
 
 export default class Header extends Component {
@@ -14,21 +16,26 @@ export default class Header extends Component {
 
   render() {
     return (
-      <header className="App-header">
-        <h1>App GitHub!</h1>
+        <header className="App-header flex-one">
+            <div className="flex-one">
+                <img src={GitHubLogo} alt="github logo" />
+                <h2>App GitHub!</h2>
+            </div>
 
-        <form
-          id="repository-form"
-          onSubmit={(event) => this.doAddRepositoryFromChild(event)}
-        >
-          <input
-            type="text"
-            name="username"
-            placeholder="digite um usuário"
-          />
-          <button type="submit">Pesquisar</button>
-        </form>
-      </header>
+            <form
+            id="repository-form"
+            onSubmit={(event) => this.doAddRepositoryFromChild(event)}
+            >
+            <input
+                type="text"
+                name="username"
+                placeholder="digite um usuário"
+            />
+            <button type="submit">Pesquisar</button>
+            </form>
+
+            <a href="https://github.com/ElderSam/react_github_API">código fonte</a>            
+        </header>
     );
   }
 }
